73dcf4402a5ec8c72861f31a3e91263eb5e07c63
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-08-19 20:37:51 -0300

    refactor: adaptado para lidar com exceções. Usando type hint nos métodos.

--- src/controllers/ProdutoController.php
+++ src/controllers/ProdutoController.php
@@ -2,14 +2,37 @@
 
 namespace CSTSI\Dbe2\controllers;
 
+use CSTSI\Dbe2\models\ProdutoModel;
+use Exception;
+
 class ProdutoController extends Controller{
 
-    public function index(){
-        echo "<br>Listar Produtos";
+    public function __construct(){
+        try{
+            $this->model = new ProdutoModel();
+        }catch(Exception $error){
+            throw $error;
+        }
     }
 
-    public function show($id){
+    public function index(){
+        // echo "<br>Listar Produtos<pre>";
+        // // print_r($this->model->read());
+        // header("Content-Type:application/json");
+        // header("Access-Control-Allow-Origin:*");//CORS
+        //MVC -> Model
+        echo json_encode($this->model->read());
+        //MVC -> View
+    }
+
+    public function show(int $id){
          echo "<br>Mostrar os dados do produto de id:$id";
+         echo "<pre>";
+         try{
+            print_r($this->model->read($id));
+         }catch(Exception $error){
+            echo "Produto de id $id não encontrado";
+         } 
     }
 
     public function create(){
@@ -20,15 +43,15 @@ class ProdutoController extends Controller{
         echo "Recebe os dados do formulário e guarda no banco";
     }
 
-    public function edit($id){
+    public function edit(int $id){
         echo "Mostrar o formulário de edição com os dados do produto de ID: $id!!";
     }
 
-    public function update($id){
+    public function update(int $id){
         echo "Recebe dados e atualiza no banco o produto de ID: $id";
     }
 
-    public function delete($id){
+    public function delete(int $id){
         echo "Mostrar um formulário de remoção com os dados do produto de ID:$id";
     }
 
