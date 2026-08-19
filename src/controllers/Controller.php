74dc611da81ec75501fc11b5eb99cf128913736a
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-08-19 20:35:56 -0300

    refactor: controller possui uma propriedade da camanda de modelo.

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -2,7 +2,13 @@
 
 namespace CSTSI\Dbe2\controllers;
 
+use CSTSI\Dbe2\interfaces\iDAO;
+use CSTSI\Dbe2\models\Model;
+
 abstract class Controller {
+
+    protected iDAO | Model $model;
+    //protected View $view;
 
     public abstract function index();
 
