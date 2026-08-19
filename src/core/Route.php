5525173ea63bcc44d25a80580870090e57f39299
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-08-19 20:37:09 -0300

    refactor: route adaptado para lidar com erros tipo 500. Usa bloco try/catch e lida com as exceções lançadas no sistema.

--- src/core/Route.php
+++ src/core/Route.php
@@ -3,55 +3,64 @@
 namespace CSTSI\Dbe2\core;
 
 use CSTSI\Dbe2\controllers\Controller;
-// use CSTSI\Dbe2\app\views\View;
+use Exception;
+
+// use CSTSI\Dbe2\views\View;
 
 class Route
 {
 
-    // localhost:porto/controlers/method/param
-    public static function resolve(array $routes): void {
-        $uriQuery = self::parseURI();
+	// localhost:porta/controllers/method/param
+	public static function resolve(array $routes): void
+	{
+		$uriQuery = self::parseURI();
 
-        $class = null;
+		$class = null;
 		$method = null;
 		$param = null;
-		
-		if ($uriQuery) {
-			$class_name = $uriQuery[0];
-			if (count($uriQuery) > 1) {
-				$method = $uriQuery[1];
-				$param = (count($uriQuery) > 2) ? $uriQuery[2] : null;
-			}
 
-			if (isset($routes[$class_name])) {
-				$class = new $routes[$class_name];//produtos
-				if ($class instanceof Controller) {
-					if ($method && method_exists($class, $method)) {
-						if ($param) {
-							$class->$method($param);
+		try {
+			if ($uriQuery) {
+				$class_name = $uriQuery[0];
+				if (count($uriQuery) > 1) {
+					$method = $uriQuery[1];
+					$param = (count($uriQuery) > 2) ? $uriQuery[2] : null;
+				}
+
+
+				if (isset($routes[$class_name])) {
+					$class = new $routes[$class_name]; //produtos
+					if ($class instanceof Controller) {
+						if ($method && method_exists($class, $method)) {
+							if ($param) {
+								$class->$method($param);
+							} else {
+								$class->$method();
+							}
 						} else {
-							$class->$method();
+							if (method_exists($class, 'index'))
+								$class->index();
+							else $class = null;
 						}
-					} else {
-						if (method_exists($class, 'index'))
-							$class->index();
-						else $class = null;
 					}
 				}
 			}
+			// if (!$class) View::pageNotFound();
+			if (!$class) header('HTTP/1.0 404 Not Found');
+		} catch (Exception $error) {
+			// echo "Erro banco!!";
+			header('HTTP/1.0 503 Servico Indisponivel!!!');
 		}
-		// if (!$class) View::pageNotFound();
-		if (!$class) header('HTTP/1.0 404 Not Found');
-    }
+	}
 
-    private static function parseURI(): array
-    {
-        if ($_SERVER['REQUEST_URI'] == '/') {
-            return [$_SERVER['REQUEST_URI']];
-        } else {
-            $url_path = trim($_SERVER['REQUEST_URI'], '/');
-            error_log("ROUTE: $url_path");
-            return explode('/', $url_path);
-        }
-    }
+	private static function parseURI(): array
+	{
+		if ($_SERVER['REQUEST_URI'] == '/') {
+			return [$_SERVER['REQUEST_URI']];
+		} else {
+			$url_path = trim($_SERVER['REQUEST_URI'], '/');
+			error_log("ROUTE: $url_path");
+			return explode('/', $url_path);
+		}
+	}
 }
