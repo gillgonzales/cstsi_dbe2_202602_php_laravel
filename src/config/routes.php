d6f3df222b2b463a036d32cc239b1a9a02ed1ef0
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-09-02 19:34:40 -0300

    feat: remoção do projeto anterior e instalação do laravel na raiz do repositório composer create-project laravel/laravel pastaMeuProjeto

--- src/config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use CSTSI\Dbe2\controllers\ProdutoController;
-use CSTSI\Dbe2\controllers\api\ProdutoController as ApiProdutoController;
-
-$routes = [
-    'produtos'=> ProdutoController::class,
-    'api/produtos'=> ApiProdutoController::class
-];
--- src/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once '../vendor/autoload.php';
-
-use CSTSI\Dbe2\core\App;
-
-App::init();
