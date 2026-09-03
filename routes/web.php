fdc9a1bad82dc0f98c356db8493998c1822076e2
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-09-02 22:18:34 -0300

    feat: configurações de rotas web.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/ola',function () {
+//     echo "Olá Mundo !!!";
+// });
+
+Route::get('/ola',[HomeController::class,'index']);
+Route::get('/produtos',[ProdutoController::class,'index']);
