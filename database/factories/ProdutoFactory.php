aaaa71fdd61d5c276b077ff48b317e024ec601fa
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-09-09 19:41:35 -0300

    feat: criação da factory de produto sail artisan make:factory ProdutoFactory -m Produto

---
+++ database/factories/ProdutoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Produto;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Produto>
+ */
+class ProdutoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nome'=>fake()->sentence(3),
+            'descricao'=>fake()->sentence(),
+            'qtd_estoque'=>fake()->numberBetween(1,100),
+            'preco'=>fake()->randomFloat(2, 1, 100),
+            'importado' => fake()->boolean()
+        ];
+    }
+}
