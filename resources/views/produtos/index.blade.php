56b8aae4eeeacff73eb1c9e0f66b5bae0bf96719
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-09-09 21:09:43 -0300

    refactor: criação de links para rotas de atualização e cadastro.

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -1,43 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Produtos</title>
 </head>
+
 <body>
     <h1>Produtos</h1>
-    @if ($produtos->count()>0)
-    <table>
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Nome</th>
-                <th>qtd_estoque</th>
-                <th>preco</th>
-                <th>Importado</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($produtos as $produto)
+    <a href={{ '/produto' }} target="_blank">
+        <h3>+Adicionar Produto!!</h3>
+    </a>
+    @if ($produtos->count() > 0)
+        <table>
+            <thead>
                 <tr>
-                    <td>
-                        <a href={{route('produto.show',$produto->id)}} target="_blank">
-                        {{-- <a href={{"/produtos/$produto->id"}} target="_blank"> --}}
-                            {{$produto->id}}
-                        </a>
-                    </td>
-                    <td>{{$produto->nome}}</td>
-                    <td>{{$produto->qtd_estoque}}</td>
-                    <td>{{$produto->preco}}</td>
-                    <td>{{($produto->importado)?'Sim':'Não'}}</td>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th>qtd_estoque</th>
+                    <th>preco</th>
+                    <th>Importado</th>
+                    <th>Ações</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($produtos as $produto)
+                    <tr>
+                        <td>
+                            <a href={{ route('produto.show', $produto->id) }} target="_blank">
+                                {{-- <a href={{"/produtos/$produto->id"}} target="_blank"> --}}
+                                {{ $produto->id }}
+                            </a>
+                        </td>
+                        <td>{{ $produto->nome }}</td>
+                        <td>{{ $produto->qtd_estoque }}</td>
+                        <td>{{ $produto->preco }}</td>
+                        <td>{{ $produto->importado ? 'Sim' : 'Não' }}</td>
+                        <td>
+                            <a href={{ route('produto.edit', $produto->id) }} target="_blank">
+                                {{-- <a href={{"/produtos/$produto->id"}} target="_blank"> --}}
+                                Editar
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @else
-     <p>Produtos não encontrados! </p>
+        <p>Produtos não encontrados! </p>
     @endif
 </body>
+
 </html>
